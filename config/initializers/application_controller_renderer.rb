57b41d0e582a61d382d0da0f9e05388b5e9e6ce1
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-12-05 01:40:47 +0000

    [controller,view]投稿機能 #1

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f0ca9a49455dddd4f79cf76697d9e58a5b4a9fe1f249c63d5f9f998906c82b3aeb4406c6900bbfb3eaee8b9fabbfe425142ac12ccbe0ab9904bd2a96a2649482'
