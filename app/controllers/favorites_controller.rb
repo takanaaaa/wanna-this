37d4573f3e5a23a76aa11adb94be7c3dd347c838
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-12-06 01:35:47 +0000

    [favorite]非同期処理

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,9 @@
 class FavoritesController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def index
@@ -13,9 +12,8 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: @post.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
